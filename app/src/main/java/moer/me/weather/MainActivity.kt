62d0dc710b00927115d60fe9279b17cd6ba3733a
Yun <yun@pikicast.com>
2016-10-06 18:29:03 +0900

    간단한 RecyclerView 사용.

--- app/src/main/java/moer/me/weather/MainActivity.kt
+++ app/src/main/java/moer/me/weather/MainActivity.kt
@@ -2,20 +2,28 @@ package moer.me.weather
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private val items = listOf(
+            "Mon 6/23 - Sunny - 31/17",
+            "Tue 6/24 - Foggy - 21/8",
+            "Wed 6/25 - Cloudy - 22/17",
+            "Thu 6/26 - Rainy - 18/11",
+            "Fri 6/27 - Foggy - 21/10",
+            "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+            "Sun 6/29 - Sunny - 20/7"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        message.text = "Hello, Kotlin!"
 
-//        toast("Hello")
-//        toast("Hello2", Toast.LENGTH_LONG)
-//        toast(message = "Hello3", length = Toast.LENGTH_SHORT)
-        niceToast("Hello, Kotlin!")
+        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+        forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.adapter = ForecastListAdapter(items)
     }
 
     fun toast(message: String, length: Int = Toast.LENGTH_SHORT) {
