391a11b4f9b961425eca1a8364739e49b817d918
Yun <yun@pikicast.com>
2016-11-22 15:08:38 +0900

    연산자 오버로딩으로 코드 간소화.

--- app/src/main/java/moer/me/weather/model/ForecastList.kt
+++ app/src/main/java/moer/me/weather/model/ForecastList.kt
@@ -3,4 +3,10 @@ package moer.me.weather.model
 /**
  * Created by Yun on 2016. 11. 22..
  */
-data class ForecastList(val city: String, val country: String, val dailyForecast:List<Forecast>)
+data class ForecastList(val city: String, val country: String, val dailyForecast:List<Forecast>) {
+
+    operator fun get(position: Int): Forecast = dailyForecast[position]
+
+    fun size(): Int = dailyForecast.size
+
+}
